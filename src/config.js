b7faf51dfa8506c3894e77cc26be0ad186762255
xuanzebin <xuanzebin@126.com>
2019-03-14 11:00:50 +0800

    fix:修复了config配置里面的类型不匹配问题

--- src/config.js
+++ src/config.js
@@ -7,16 +7,16 @@ import { O2_AMBIENT_CONFIG } from './js/utils/const'
 
 window[O2_AMBIENT_CONFIG] = {
   particleNumber: 60,
-  textures: {
-    texture1: snow1,
-    texture2: snow2,
-    texture3: snow3,
-    texture4: snow3,
-    texture5: snow3,
-    texture6: snow3,
-    texture7: snow3,
-    texture8: snow4
-  },
+  textures: [
+    snow1,
+    snow2,
+    snow3,
+    snow3,
+    snow3,
+    snow3,
+    snow3,
+    snow4
+  ],
   duration: 0,
   endCallback: encodeURI((() => {
     console.log('雪花落完了')
