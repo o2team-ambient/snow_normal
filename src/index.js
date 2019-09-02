b2e74d578ec6c94a613352e07306568bd35a2551
Lee EC <lyxuncle@gmail.com>
2019-09-02 21:31:31 +0800

    feat: 更新上报 ID

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ try {
 
 const handleReport = () => {
   Report.init({})
-  Report.processPV()
+  Report.processPV(21)
 }
 
 if (typeof window.XView === 'undefined') {
---
+++ src/js/configs/configVelantine.js
@@ -0,0 +1,12 @@
+import { O2_AMBIENT_CONFIG } from '../utils/const'
+
+export default {
+  ...window[O2_AMBIENT_CONFIG],
+  particleNumber: 25,
+  textures: [
+    'https://m.360buyimg.com/babel/jfs/t1/44242/40/11377/452/5d497597E5b665905/6c5b2329f0d87fe6.png',
+    'https://m.360buyimg.com/babel/jfs/t1/80660/19/6526/808/5d4975a4E852abf2b/73ee4afd184658f3.png',
+    'https://m.360buyimg.com/babel/jfs/t1/63411/21/6422/1203/5d4975aeEe2db5a67/ccfc4ed75e7944b6.png'
+  ],
+  duration: 6
+}
